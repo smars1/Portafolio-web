f9d08480a6a7eeea6fa65f8dabcd2f96aa7f36a9
DAPC98 <diego198mayo@gmail.com>
2022-09-08 13:16:31 -0700

    Update main.js
    Added Js Code in the main.js file

--- JS_Project/Todo_List/main.js
+++ JS_Project/Todo_List/main.js
@@ -1,4 +1,13 @@
 // window.onload permite que cargue el html antes que el JS
 window.onload = () => {
-
+// Creamos form el cual llamada al elemento form por su ID
+const form = document.getElementById('todo-form');
+form.onsubmit = (refresh) => {
+    // los submit tienen como determinado resfresh eso lo podemos cambiar con la siguiente funcion
+    refresh.preventDefault();
+    const todo = document.getElementById('todo');
+    const todoText = todo.value;
+    todo.value = '';
+    console.log(todoText);
+    }
 }
