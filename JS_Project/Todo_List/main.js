5a9ae7296a5ed288e57449a33b5385ac3c8da16e
DAPC98 <diego198mayo@gmail.com>
2022-09-08 13:38:19 -0700

    Update main.js

--- JS_Project/Todo_List/main.js
+++ JS_Project/Todo_List/main.js
@@ -4,9 +4,12 @@ window.onload = () => {
 const form = document.getElementById('todo-form');
 form.onsubmit = (refresh) => {
     // los submit tienen como determinado resfresh eso lo podemos cambiar con la siguiente funcion
-    refresh.preventDefault();
+    refresh.preventDefault();// prevenimos que el navegador se refresque
+    // vamos a input id = "todo"
     const todo = document.getElementById('todo');
+    // Sacamos el valor de input id = "todo"
     const todoText = todo.value;
+    // Cambiamos el valor de input id todo y lo remplazamos por un string vacio
     todo.value = '';
     console.log(todoText);
     }
