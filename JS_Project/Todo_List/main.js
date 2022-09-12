9044decb8cc8f3fbea2af67b4d5c087fd01f17c9
DAPC98 <diego198mayo@gmail.com>
2022-09-12 10:53:06 -0700

    I create the render function

--- JS_Project/Todo_List/main.js
+++ JS_Project/Todo_List/main.js
@@ -1,6 +1,29 @@
 // Creamos un arreglo al que se le iran agragando elementos
 const todos = []; // array empty
 
+// Creamos funcion render
+const render = () => {
+    const todoList = document.getElementById('todo-list');
+    // limpiamos el listado, evita repeticion de elementos 
+    todoList.innerHTML = '';
+
+    const todoTemplate = todos.map(t => '<li>' + t + '</li>');
+    todoList.innerHTML = todoTemplate.join('');
+
+    // Seleccionamos todos lo elementos de todo-list que sean li
+    const elementos = document.querySelectorAll('#todo-list li');
+    // iteramos con la funcion de forEach 
+    elementos.forEach((elementos, i) => {
+        // Agregramos un escuchador de evento con el metodo .addEventListener 
+            elementos.addEventListener('click', () => {
+                // removemos un nodo hijo de uno padre
+                elementos.parentNode.removeChild(elementos);
+                //indicamos el indice del elemento a eliminar de del array (limpiamos el array)
+                todos.splice(i, 1);     
+            });
+        });
+}
+
 // window.onload permite que cargue el html antes que el JS
 window.onload = () => {
 // Creamos form el cual llamada al elemento form por su ID
@@ -17,18 +40,8 @@ form.onsubmit = (refresh) => {
 
     // .push nos permite agregar elementos al array
     todos.push(todoText);
-    const todoList = document.getElementById('todo-list');
-    // limpiamos el listado, evita repeticion de elementos 
-    todoList.innerHTML = '';
 
-    const todoTemplate = todos.map(t => '<li>' + t + '</li>');
-    todoList.innerHTML = todoTemplate.join('');
-
-    const elementos = document.querySelectorAll('#todo-list li');
-    elementos.forEach((elementos, i) => {
-            elementos.addEventListener('click', () => {
-                console.log(elementos, i);
-            });
-        });
+    // Ejecutamos render
+    render();
     }
 }
